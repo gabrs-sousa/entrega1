e6209947941b9cb4751658da706ac1f4dde83c80
gabrs-sousa <gabriel_rsousa@hotmail.com>
2024-10-16 21:42:28 -0300

    Adicionando a página

---
+++ Arq1.js
@@ -0,0 +1,8 @@
+var nome = prompt("Qual é seu nome?")
+var idade = prompt("Qual é sua idade?")
+
+if (idade<18) {
+    alert(nome+", você é menor de idade!")
+}else {
+    alert(nome+", você é maior de idade!")
+}
---
+++ js-curso-2-aula1/app.js
@@ -0,0 +1,5 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo
